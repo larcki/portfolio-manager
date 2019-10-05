f63b7bd04c2edecf40c6710d0aca156a031e52af
larcki <larcki@users.noreply.github.com>
2019-10-05 16:06:00 +0100

    Added vuetify

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,36 @@
 <template>
+    <v-app>
 
-    <div id="app">
-        <portfolio-chart initial-period="180" initial-tags="BOND,STOCK"/>
-    </div>
+        <v-app-bar app>
+            <v-toolbar-title class="headline text-uppercase">
+                <span class="font-weight-light">Asset Portfolio</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn text
+                   href="https://github.com/larcki/portfolio-manager"
+                   target="_blank">
+                <span class="mr-2">GitHub</span>
+            </v-btn>
+        </v-app-bar>
 
+        <v-content>
+            <v-container fluid>
+                <v-row>
+                    <v-col>
+                        <v-card :elevation="n - 1">
+                            <portfolio-chart initial-period="180" initial-tags="BOND,STOCK"/>
+                        </v-card>
+                    </v-col>
+                    <v-col>
+                        <v-card :elevation="n - 1">
+                            <portfolio-chart initial-period="180" initial-tags="BOND,STOCK"/>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-content>
+
+    </v-app>
 </template>
 
 <script>
@@ -16,14 +43,3 @@
         }
     }
 </script>
-
-<style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
-</style>
