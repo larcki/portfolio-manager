f6890483c500a6188c85bc9edfff78f5d311b608
larcki <larcki@users.noreply.github.com>
2019-12-25 17:46:29 +0000

    Dashboard layout

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -3,21 +3,21 @@
         <v-container fluid>
             <v-row>
 
-                <v-col cols="12" md="8">
+                <v-col cols="12" lg="8">
                     <v-card :elevation="n - 1">
                         <chart-selector></chart-selector>
                     </v-card>
                 </v-col>
-                <v-col cols="4">
+
+                <v-col cols="12" lg="4">
+
                     <v-card :elevation="n - 1">
                         <pie-chart asset-classes="BOND,STOCK,PROPERTY"/>
                     </v-card>
+                    <div class="summary-card">
+                        <summary-tile></summary-tile>
+                    </div>
 
-                </v-col>
-            </v-row>
-            <v-row>
-                <v-col cols="4">
-                    <summary-tile></summary-tile>
                 </v-col>
             </v-row>
 
@@ -38,3 +38,8 @@
         }
     }
 </script>
+<style>
+    .summary-card {
+        padding-top: 24px;
+    }
+</style>
